2b7f98d15089ae9215e941d04bab21b4edb7a2e1
matheus.h.paula6@aluno.senai.br <45540504836@escola927.edu>
2025-06-10 11:25:42 -0300

    meu primeiro commit

---
+++ script.js
@@ -0,0 +1,12 @@
+const btnAdd = document.getElementById("btn-add");
+const tarefa = document.getElementById("tarefa");
+const taskList = document.getElementById("taskList");
+
+let nome = prompt("Qual o seu nome?")
+titulo.innerHTML = `Lista de tarefa: ${nome}`;
+
+btnAdd.addEventListener("click", criarTarefa)
+
+function criarTarefa(){
+    
+}
